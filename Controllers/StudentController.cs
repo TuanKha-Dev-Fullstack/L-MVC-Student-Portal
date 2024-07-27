7d66e14256a56047ec5d87cb9c157eefe467e2f2
TuanKha-Dev-Fullstack <tuankha.dev@gmail.com>
2024-07-27 10:56:23 +0700

    feat: add new student
    - create student controller
    - create view model for student
    - create view for added new student

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+using L_MVC_Student_Portal.Data;
+using L_MVC_Student_Portal.Models.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L_MVC_Student_Portal.Controllers
+{
+    public class StudentController(AppDbContext _context) : Controller
+    {
+        // GET: StudentController
+        [HttpGet]
+        public ActionResult Add()
+        {
+            return View();
+        }
+        
+        // POST: StudentController
+        [HttpPost]
+        public async Task<ActionResult> Add(Models.AddStudentViewModel studentInfo)
+        {
+            var student = new Student()
+            {
+                Name = studentInfo.Name,
+                Email = studentInfo.Email,
+                Phone = studentInfo.Phone,
+                Subscribed = studentInfo.Subscribed
+            };
+            _context.Students.Add(student);
+            await _context.SaveChangesAsync();
+            return View();
+        }
+    }
+}
---
+++ Models/AddStudentViewModel.cs
@@ -0,0 +1,12 @@
+namespace L_MVC_Student_Portal.Models;
+
+public class AddStudentViewModel
+{
+    public string Name { get; set; }
+
+    public string Email { get; set; }
+
+    public string Phone { get; set; }
+
+    public bool Subscribed { get; set; }
+}
